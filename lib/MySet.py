71af6f545f18ea49413822cd7690c0972acf10ee
Raddames <raddamestonui48@gmail.com>
2024-06-06 01:30:56 +0300

    done

--- lib/MySet.py
+++ lib/MySet.py
@@ -1,25 +1,29 @@
 class MySet:
+
     def __init__(self, enumerable = []):
         self.dictionary = {}
         for value in enumerable:
             self.dictionary[value] = True
-    
+
     def has(self, value):
         return value in self.dictionary
-    
+
+    def __str__(self):
+        set_list = []
+        for key, value in self.dictionary.items():
+            set_list.append(str(key))
+        return f'MySet: {{{",".join(set_list)}}}'
+
     def add(self, value):
-        if value not in self.dictionary:
-            self.dictionary[value] = True
-            return (f"{value} added")
-        else:
-            return (f"{value} already in set")
-    
+        self.dictionary[value] = True # Add a value as a key on the Dictionary
+        return self
+
     def delete(self, value):
         self.dictionary.pop(value, None)
         return self
-    
+
     def size(self):
         return len(self.dictionary)
 
-my_list = MySet([1,2,3,4,4,3,4,6,6,1])
-print(my_list.add(6))
+    def clear(self):
+        self.dictionary.clear()
