a00bb870628358813c38cc1e90dc4ccd582497ae
Raddames <raddamestonui48@gmail.com>
2024-06-06 01:33:40 +0300

    doen

--- lib/MySet.py
+++ lib/MySet.py
@@ -1,6 +1,5 @@
 class MySet:
-
-    def __init__(self, enumerable = []):
+    def __init__(self, enumerable=[]):
         self.dictionary = {}
         for value in enumerable:
             self.dictionary[value] = True
@@ -15,7 +14,7 @@ class MySet:
         return f'MySet: {{{",".join(set_list)}}}'
 
     def add(self, value):
-        self.dictionary[value] = True # Add a value as a key on the Dictionary
+        self.dictionary[value] = True  # Add a value as a key in the dictionary
         return self
 
     def delete(self, value):
@@ -27,3 +26,9 @@ class MySet:
 
     def clear(self):
         self.dictionary.clear()
+
+# Example usage:
+my_list = MySet([1, 2, 3, 4, 4, 3, 4, 6, 6, 1])
+print(my_list.add(6))  # Output: <__main__.MySet object at 0x...>
+print(my_list.delete(9))  # Output: <__main__.MySet object at 0x...>
+print(my_list.size())  # Output: 5
