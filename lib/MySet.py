f3848714c82aebe8ef7517bf0ab5466c030e8656
Raddames Tonui <raddamestonui48@gmail.com>
2025-02-12 13:28:30 +0300

    Update MySet.py

--- lib/MySet.py
+++ lib/MySet.py
@@ -1,34 +1,88 @@
+def first_repeated_value(list):
+  # create a Set to keep track of values we've seen
+  number_set = set()
+  # iterate over each element from the list
+  for i in range (0, len(list)):
+
+    # if we've already seen a value, we've found the duplicate!
+    if list[i] in number_set:
+        return list[i]
+    # otherwise, add the value to our set
+    number_set.add(list[i])
+  # return None if we reach the end and haven't found our value
+  return None
+
+print(first_repeated_value([1,2,3,3,4,4]))
+# => 3
+
+# USING DICTIONARY
+    
+class MyDict:        
+    def __init__(self, enumerable=[]):  
+        self.dictionary = {}  
+        for value in enumerable:  
+            self.dictionary[value] = True  # Store unique values as keys  
+    
+    def __repr__(self):  
+        return f"MyDict({list(self.dictionary.keys())})"  # Corrected the name from MySet to MyDict  
+    
+    def has(self, value):  
+        return value in self.dictionary  # Check for the presence of value  
+      
+    def add(self, value):  
+        self.dictionary[value] = True  # Add unique value  
+        return self  # Method chaining enabled  
+    
+    def delete(self, value):  
+        self.dictionary.pop(value, None)  # Safely remove value  
+        return self  
+    
+    def size(self):  
+        return len(self.dictionary)  # Number of unique values  
+    
+    def clear(self):  
+        self.dictionary.clear()  # Clear all elements  
+        return self
+
+# USING SETS
+
 class MySet:
-    def __init__(self, enumerable=[]):
-        self.dictionary = {}
-        for value in enumerable:
-            self.dictionary[value] = True
+    def __init__(self, enumerable =[]):
+      self.set = set()
+      for value in enumerable:
+         self.set.add(value)
 
     def has(self, value):
-        return value in self.dictionary
-
-    def __str__(self):
-        set_list = []
-        for key, value in self.dictionary.items():
-            set_list.append(str(key))
-        return f'MySet: {{{",".join(set_list)}}}'
+        return value in self.set
 
     def add(self, value):
-        self.dictionary[value] = True  # Add a value as a key in the dictionary
-        return self
+       self.set.add(value)
+       return self
 
     def delete(self, value):
-        self.dictionary.pop(value, None)
-        return self
-
+       self.set.discard(value)              
+       return self
+    
     def size(self):
-        return len(self.dictionary)
-
+       return len(self.set)
+    
     def clear(self):
-        self.dictionary.clear()
+       self.set.clear()
+       return  self
+    
+    def __repr__(self):
+       return f"MySet({list(self.set)})"
+       
 
-# Example usage:
-my_list = MySet([1, 2, 3, 4, 4, 3, 4, 6, 6, 1])
-print(my_list.add(6))  # Output: <__main__.MySet object at 0x...>
-print(my_list.delete(9))  # Output: <__main__.MySet object at 0x...>
-print(my_list.size())  # Output: 5
+dset = MyDict([1,2,3,5,6,4,2,3])
+print(dset.dictionary)
+print(dset.add(7))
+print(dset)
+print(dset.clear())
+print(f"\n")
+
+myset =MySet([1,2,3,5,6,4,2,3])
+print(myset.set)
+print(myset.add(7))
+print(myset)
+print(myset.clear())
